affc8280ef2fbfaecf3cc67b86ea35564200f907
Admin <GA-Admin@Admins-MacBook-Pro-2.local>
2018-07-11 12:59:25 -0500

    setup complete

---
+++ 07_11_2018_pairs_lab/models/persons.js
@@ -0,0 +1,76 @@
+const persons = [{
+  "id": 1,
+  "first_name": "Baxter",
+  "last_name": "Burwin",
+  "email": "bburwin0@ifeng.com",
+  "gender": "Male",
+  "ip_address": "169.102.150.178"
+}, {
+  "id": 2,
+  "first_name": "Tab",
+  "last_name": "Mapples",
+  "email": "tmapples1@elegantthemes.com",
+  "gender": "Male",
+  "ip_address": "1.70.128.93"
+}, {
+  "id": 3,
+  "first_name": "Jud",
+  "last_name": "De Blasio",
+  "email": "jdeblasio2@jimdo.com",
+  "gender": "Male",
+  "ip_address": "192.128.135.140"
+}, {
+  "id": 4,
+  "first_name": "Odey",
+  "last_name": "Bolesworth",
+  "email": "obolesworth3@ucoz.com",
+  "gender": "Male",
+  "ip_address": "1.190.185.71"
+}, {
+  "id": 5,
+  "first_name": "Dell",
+  "last_name": "Iddens",
+  "email": "diddens4@yolasite.com",
+  "gender": "Male",
+  "ip_address": "89.206.35.182"
+}, {
+  "id": 6,
+  "first_name": "Teresa",
+  "last_name": "Darbishire",
+  "email": "tdarbishire5@sitemeter.com",
+  "gender": "Female",
+  "ip_address": "67.5.173.132"
+}, {
+  "id": 7,
+  "first_name": "Heywood",
+  "last_name": "Casse",
+  "email": "hcasse6@fema.gov",
+  "gender": "Male",
+  "ip_address": "243.15.176.139"
+}, {
+  "id": 8,
+  "first_name": "Sal",
+  "last_name": "Jannasch",
+  "email": "sjannasch7@marketwatch.com",
+  "gender": "Male",
+  "ip_address": "135.18.144.217"
+}, {
+  "id": 9,
+  "first_name": "Helena",
+  "last_name": "Glisenan",
+  "email": "hglisenan8@sciencedirect.com",
+  "gender": "Female",
+  "ip_address": "29.195.131.46"
+}, {
+  "id": 10,
+  "first_name": "Adrian",
+  "last_name": "Beardall",
+  "email": "abeardall9@forbes.com",
+  "gender": "Male",
+  "ip_address": "83.97.88.186"
+}]
+
+
+
+
+module.exports = persons
