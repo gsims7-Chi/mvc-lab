826d776c3d5bd33d349def7c7dbb55a93e42a0e0
Admin <GA-Admin@Admins-MacBook-Pro-2.local>
2018-07-11 13:18:41 -0500

    created an array of objects

--- 07_11_2018_pairs_lab/server.js
+++ 07_11_2018_pairs_lab/server.js
@@ -1,3 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({extend: true}));
+app.use(methodOverride('_method'));
+
+
+const persons = require('./controllers/controllers')
+
+// this means that every route in the fruit controller starts with / fruits
+app.use('/persons', persons)
+
+
+app.listen(7777, () => {
+  console.log("I'm over 7777");
+})
